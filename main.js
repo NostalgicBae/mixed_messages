dee52751c065912934393bb541c3b8962896fac8
Nostalgicbae <nostalgicbae@MSI.localdomain>
2020-12-30 23:46:47 +0100

    created randMessage function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const animals = ['frog', 'cat', 'cow', 'crocodile', 'monkey', 'bull', 'butterfly', 'dog'];
 const buildings = ['house', 'chalet', 'tree-house', 'bungalow', 'yourt', 'tipee', 'mansion'];
 const countries = ['France', 'Germany', 'Japan', 'Canada', 'Argentina', 'Kenya', 'Turkey'];
+
+const randMessage = () => {
+
+};
