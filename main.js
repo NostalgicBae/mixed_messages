37225207db258a514baf73c54091a06838df58fc
Nostalgicbae <nostalgicbae@MSI.localdomain>
2020-12-30 23:41:51 +0100

    created arrays for data

---
+++ main.js
@@ -0,0 +1,3 @@
+const animals = ['frog', 'cat', 'cow', 'crocodile', 'monkey', 'bull', 'butterfly', 'dog'];
+const buildings = ['house', 'chalet', 'tree-house', 'bungalow', 'yourt', 'tipee', 'mansion'];
+const countries = ['France', 'Germany', 'Japan', 'Canada', 'Argentina', 'Kenya', 'Turkey'];
